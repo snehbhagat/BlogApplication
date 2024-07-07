9a40baab66b0ff26f3b1c642db6f6259592b4897
Sneh Bhagat <snehbhagat12@gmail.com>
2024-07-07 13:52:17 +0530

    set up basic structure of app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Temporary data store
+let posts = [];
+
+app.listen(port, () => {
+    console.log(`Blog app listening at http://localhost:${port}`);
+});
