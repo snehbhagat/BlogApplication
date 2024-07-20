fb5a20a7b1f909e8027309f7dab1b02ab0373fdc
Sneh Bhagat <snehbhagat12@gmail.com>
2024-07-20 11:04:02 +0530

    Added path for home page

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
+import { dirname } from 'path';
+import { fileURLToPath } from "url";
+
 const app = express();
 const port = 3000;
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 //Setting the main route
 app.get('/', (req,res) => {
-  res.send("Hello World");
+  res.render(__dirname + '/views/pages/index.ejs');
 })
+
+console.log(__dirname);
 
 //Starting the server
 app.listen(port, () => {
